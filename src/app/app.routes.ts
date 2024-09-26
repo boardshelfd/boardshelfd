99ef7592c1a1572ec330ff6c0fd785a0b822e2ba
axdelafuen <axel.de_la_fuente@etu.uca.fr>
2024-09-26 18:12:09 +0200

    update routing + title

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AppNotfoundComponent } from './pages/app.notfound.component';
+import { HomeComponent } from './domain/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home',  pathMatch: 'full'},
+    { 
+        path: '', 
+        component: AppComponent,
+        children: [
+            {
+                path: '',
+                redirectTo: 'home',
+                pathMatch: 'full',
+            },
+            {
+                path: 'home',
+                component: HomeComponent,
+            }
+        ]
+    },
+    { path: 'notfound', component: AppNotfoundComponent },
+    { path: '**', redirectTo: '/notfound' },
+];
